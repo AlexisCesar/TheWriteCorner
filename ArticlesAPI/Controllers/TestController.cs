594d5af89d1eb45967baff2648c72287d8a2d829
Alexis Almeida <alexis.cesar2017@outlook.com>
2023-06-26 17:48:06 -0300

    Add MongoDBDriver dependency

---
+++ ArticlesAPI/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArticlesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet(Name = "GetValueFromDatabase")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+        public IActionResult Get()
+        {
+            return Ok("Value from database: ...");
+        }
+    }
+}
