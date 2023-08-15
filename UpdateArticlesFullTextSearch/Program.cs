51bb91e00ae9d9ab533d899fa09760aaef78641d
bruna.goncalves <pissara2@gmail.com>
2023-08-15 18:15:07 -0300

    Adding UpdateArticlesFullTextSearch service

---
+++ UpdateArticlesFullTextSearch/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using UpdateArticlesFullTextSearch.RabbitMq;
+
+class Program
+{
+    static void Main()
+    {
+        var configuration = new ConfigurationBuilder()        
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var rabbitMqConnection = new RabbitMqConnection(configuration);
+        var rabbitMqConsumer = new RabbitMqConsumer(rabbitMqConnection);
+
+        Console.WriteLine("Starting consumer");
+
+        rabbitMqConsumer.RabbitMqStartConsumer("updateArticle", "articlesOperations");
+
+        Console.WriteLine("Ending application");    
+    }
+}
---
+++ UpdateArticlesFullTextSearch/RabbitMq/RabbitMqConsumer.cs
@@ -0,0 +1,52 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace UpdateArticlesFullTextSearch.RabbitMq
+{
+    public interface IRabbitMqPublisher
+    {
+        void PublishMessage(string exchange, string message);
+    }
+    public class RabbitMqConsumer
+    {
+
+        IRabbitMqConnection _rabbitMqConnection;
+        public RabbitMqConsumer(IRabbitMqConnection rabbitMqConnection)
+        {
+            _rabbitMqConnection = rabbitMqConnection;
+        }
+
+        public void RabbitMqStartConsumer(string queueName, string exchange) 
+        {
+            var channel = _rabbitMqConnection.CreateChannel();
+            
+            channel.ExchangeDeclare(exchange: exchange,
+                                    type: ExchangeType.Fanout,
+                                    durable: true,
+                                    autoDelete: false);
+
+            channel.QueueDeclare(queueName, true, false, false, null);
+
+            channel.QueueBind(queue: queueName,
+                                exchange: exchange,
+                                routingKey: string.Empty);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                var routingKey = ea.RoutingKey;
+                Console.WriteLine($" [x] Received '{routingKey}':'{message}'");
+
+                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            };
+
+            channel.BasicConsume(queue: queueName,
+                                    autoAck: false,
+                                    consumer: consumer);            
+        }
+    }
+}
