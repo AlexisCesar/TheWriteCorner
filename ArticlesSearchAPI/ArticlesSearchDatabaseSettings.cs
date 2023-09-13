e6758285ad9377cc1de9e5d857526b56778be296
Alexis Almeida <alexis.cesar2017@outlook.com>
2023-09-13 20:58:20 -0300

    Add ArticlesSearchAPI project

---
+++ ArticlesSearchAPI/ArticlesSearchDatabaseSettings.cs
@@ -0,0 +1,8 @@
+﻿public class ArticlesSearchDatabaseSettings
+{
+    public string ConnectionString { get; set; } = null!;
+
+    public string DatabaseName { get; set; } = null!;
+
+    public string ArticlesCollectionName { get; set; } = null!;
+}
---
+++ ArticlesSearchAPI/ArticlesService.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+public interface IArticlesService
+{
+    Task<List<Article>> GetAsync();
+    Task<Article?> GetAsync(string id);
+}
+public class ArticlesService : IArticlesService
+{
+    private readonly IMongoCollection<Article> _articlesCollection;
+
+    public ArticlesService(IOptions<ArticlesSearchDatabaseSettings> articlesDatabaseSettings)
+    {
+        var mongoClient = new MongoClient(
+            articlesDatabaseSettings.Value.ConnectionString);
+
+        var mongoDatabase = mongoClient.GetDatabase(
+            articlesDatabaseSettings.Value.DatabaseName);
+
+        _articlesCollection = mongoDatabase.GetCollection<Article>(
+            articlesDatabaseSettings.Value.ArticlesCollectionName);
+    }
+
+    public async Task<List<Article>> GetAsync() =>
+        await _articlesCollection.Find(_ => true).ToListAsync();
+
+    public async Task<Article?> GetAsync(string id) =>
+        await _articlesCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+}
