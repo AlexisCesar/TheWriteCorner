a9764ff5bea6e998b191d311650c0bfa8f44c0c0
bruna.goncalves <pissara2@gmail.com>
2023-09-29 16:14:04 -0300

    Implementation auth api

---
+++ AuthAPI/Data/DTOs/CreateUserDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthAPI.Data.DTOs
+{
+    public class CreateUserDto
+    {
+        [Required]
+        public string Username { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
+
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        public string PasswordConfirmation { get; set; }
+    }
+}
---
+++ AuthAPI/Services/TokenService.cs
@@ -0,0 +1,42 @@
+﻿using AuthAPI.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthAPI.Services
+{
+    public class TokenService
+    {
+        private IConfiguration _configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(User usuario)
+        {
+            Claim[] claims = new Claim[]
+            {
+                new Claim("username", usuario.UserName),
+                new Claim("id", usuario.Id),     
+                new Claim("loginTimestamp", DateTime.UtcNow.ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("9EkdX4IwbqZPScUzFNZpB7OIJkMlWJf9ceYfbZgHqt8=\r\n"));
+
+            var signingCredentials =
+                new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken
+                (
+                expires: DateTime.Now.AddMinutes(10),
+                claims: claims,
+                signingCredentials: signingCredentials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
