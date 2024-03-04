2af54bbfc4f109e75d4c9bdbbc45d45115006dc2
Alexis Almeida <alexis.cesar2017@outlook.com>
2024-03-04 20:51:16 -0300

    Fix issues when running containers

--- SendArticleNotification/Data/EmailDbContext.cs
+++ SendArticleNotification/Data/EmailDbContext.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics.Contracts;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using SendArticleNotification.Models;
 
 namespace SendArticleNotification.Data
